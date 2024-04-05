ee7e383ce682de411327f31106eac0062b8bfa36
EddyEhhh <ehhitseddy@gmail.com>
2024-04-05 14:15:02 +0800

    add: building movement and drop

--- app/src/main/java/com/example/stacker/ui/screens/GameScreen.kt
+++ app/src/main/java/com/example/stacker/ui/screens/GameScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.AlertDialog
@@ -18,6 +19,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -28,6 +30,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -42,24 +45,24 @@ fun GameScreen(navController: NavController){
 
     var isTap by remember { mutableStateOf(false) }
     var isDoubleTap by remember { mutableStateOf(false) }
+    var isPause: MutableState<Boolean> = remember { mutableStateOf(false) }
+
 
     // Game Variable
-    var buildingX by remember { mutableIntStateOf(0) }
-    var buildingY by remember { mutableIntStateOf(0) }
 
 
 
     val haptic = LocalHapticFeedback.current
 
-    if (isTap) {
-        AlertDialog(
-            onDismissRequest = { isTap = false },
-            title = { Text("Ni Hao") },
-            text = { Text("You tapped") },
-            confirmButton = { Button(onClick = { isTap = false }) { Text("OK") } }
-        )
-
-    }
+//    if (isTap) {
+//        AlertDialog(
+//            onDismissRequest = { isTap = false },
+//            title = { Text("Ni Hao") },
+//            text = { Text("You tapped") },
+//            confirmButton = { Button(onClick = { isTap = false }) { Text("OK") } }
+//        )
+//
+//    }
 
     if (isDoubleTap) {
         AlertDialog(
@@ -74,7 +77,7 @@ fun GameScreen(navController: NavController){
     }
 
 
-
+    println("HELLO"+isPause)
 
     // View
     Column(
@@ -87,35 +90,87 @@ fun GameScreen(navController: NavController){
                 )
             }
     ) {
-        PauseComponent(navController)
-        GameBuilding()
+        PauseComponent(navController, isPause)
+
+        val BUILDING_WIDTH = 100
+        val BUILDING_INIT_DIFFICULTY = 50
+        val BUILDING_INIT_X_OFFSET = 0
+        val BUILDING_INIT_Y_OFFSET = 50
+        val BUILDING_DROP_STOP_Y_OFFSET = getScreenHeight()/2
+
+        var buildingXOffset by remember { mutableIntStateOf(BUILDING_INIT_X_OFFSET) }
+        var buildingYOffset by remember { mutableIntStateOf(BUILDING_INIT_Y_OFFSET) }
+        var isMovingRight by remember { mutableStateOf(true) }
+        var isBuildingDropping by remember { mutableStateOf(false) }
+        var buildingMovementSpeed by remember { mutableIntStateOf(5) }
+        var buildingDropSpeed by remember { mutableIntStateOf(5) }
+
+
+
+        if(!isPause.value &&
+            !isBuildingDropping &&
+            isMovingRight &&
+            buildingXOffset < getScreenWidth()-BUILDING_WIDTH
+            ){
+            buildingXOffset += buildingMovementSpeed;
+        }else if(!isPause.value &&
+            !isBuildingDropping
+            ){
+            isMovingRight = false
+        }
+
+        if(!isPause.value &&
+            !isBuildingDropping &&
+            !isMovingRight &&
+            buildingXOffset > 0
+            ){
+            buildingXOffset -= buildingMovementSpeed;
+        }else if(!isPause.value &&
+            !isBuildingDropping
+            ){
+            isMovingRight = true
+        }
+
+        if(!isPause.value && isBuildingDropping && buildingYOffset <= BUILDING_DROP_STOP_Y_OFFSET){
+            buildingYOffset += buildingDropSpeed
+        }else if(!isPause.value ){
+            isBuildingDropping = false
+        }
+
+        if (!isPause.value && isTap) {
+            isBuildingDropping = true
+        }
+
+
+        Image(
+            modifier = Modifier
+                .offset(x = buildingXOffset.dp, y = buildingYOffset.dp)
+                .size(BUILDING_WIDTH.dp),
+            contentDescription = null, // Set your content description here
+            painter = painterResource(id = com.example.stacker.R.drawable.building_base)
+        )
 
     }
 }
 
-@Composable
-fun GameBuilding() {
-
-    Canvas(modifier = Modifier
-        .size(100.dp)
-        .padding(20.dp)
-    ) {
-    }
-}
+//@Composable
+//fun GameBuilding(nacController: NavController, isPause: MutableState<Boolean>) {
+//
+//
+//
+//}
 
 @Composable
-fun PauseComponent(navController: NavController){
+fun PauseComponent(navController: NavController, isPause: MutableState<Boolean>){
 
-    var isPause by remember { mutableStateOf(false) }
     var isPauseButtonActive by remember { mutableStateOf(false) }
 
-
-    if (isPause) {
+    if (isPause.value) {
         AlertDialog(
-            onDismissRequest = { isPause = false; isPauseButtonActive = false; },
+            onDismissRequest = { isPause.value = false; isPauseButtonActive = false; },
             title = { Text("Game Paused!") },
-            dismissButton = { Button(onClick = { isPause = false; isPauseButtonActive = false; }) { Text("Continue") } },
-            confirmButton = { Button(onClick = { isPause = false; navController.navigate(Screen.TitleScreen.route) }) { Text("I QUIT!") } }
+            dismissButton = { Button(onClick = { isPause.value = false; isPauseButtonActive = false; }) { Text("Continue") } },
+            confirmButton = { Button(onClick = { isPause.value = false; navController.navigate(Screen.TitleScreen.route) }) { Text("I QUIT!") } }
         )
 
     }
@@ -128,6 +183,7 @@ fun PauseComponent(navController: NavController){
     }
 
 
+
     Surface(
         color = Color.Transparent,
         modifier = Modifier
@@ -136,7 +192,7 @@ fun PauseComponent(navController: NavController){
             .clickable(
                 interactionSource = interactionSource,
                 indication = null,
-                onClick = { isPauseButtonActive = true; isPause = true; },
+                onClick = { isPauseButtonActive = true; isPause.value = true; },
             )
     ){
         Image(
@@ -148,4 +204,17 @@ fun PauseComponent(navController: NavController){
 
         )
     }
+
+
+}
+
+@Composable
+fun getScreenHeight(): Int {
+    val configuration = LocalConfiguration.current
+    return configuration.screenHeightDp
+}
+@Composable
+fun getScreenWidth(): Int {
+    val configuration = LocalConfiguration.current
+    return configuration.screenWidthDp
 }
