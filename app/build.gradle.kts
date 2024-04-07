5cb344d4c20e425f32123d58fb8dec625632023b
Ben <ashencheer@gmail.com>
2024-04-07 17:40:42 +0800

    make notification after recording score

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,5 +72,5 @@ dependencies {
 
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
-
+    implementation("com.google.accompanist:accompanist-permissions:0.31.1-alpha")
 }
--- app/src/main/java/com/example/stacker/ui/screens/RecordScoreScreen.kt
+++ app/src/main/java/com/example/stacker/ui/screens/RecordScoreScreen.kt
@@ -1,5 +1,12 @@
 package com.example.stacker.ui.screens
 
+import android.Manifest
+import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
@@ -22,9 +29,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.database.FirebaseDatabase
 
 
@@ -36,6 +47,7 @@ fun RecordScoreScreen(navController: NavController, score: Int) {
         id = R.color.background
     )
     var text by remember { mutableStateOf("Anonymous") }
+    val context = LocalContext.current
 
     Box(
         modifier = Modifier
@@ -75,6 +87,7 @@ fun RecordScoreScreen(navController: NavController, score: Int) {
 
             FloatingActionButton(
                 onClick = {
+                    showNotification(context)
                     submitScore(name = text, score = score)
                     navController.navigate(Screen.TitleScreen.route) },
             ) {
@@ -99,3 +112,43 @@ fun submitScore(name: String, score: Int) {
         .addOnSuccessListener { println("Insertion successful") }
         .addOnFailureListener { println("Insertion failed") }
 }
+
+fun showNotification(context: Context) {
+    val channelId = "hdbuilder_notification"
+    val notificationId = 1
+    val notificationManager = NotificationManagerCompat.from(context)
+
+    // create notification channel
+    val channel = NotificationChannel(
+        channelId,
+        "hdbuilder",
+        NotificationManager.IMPORTANCE_HIGH
+    ).apply {
+        description = "This is HDBuilder's channel"
+    }
+    notificationManager.createNotificationChannel(channel)
+
+    // create notification
+    val builder = NotificationCompat.Builder(context, channelId)
+        .setSmallIcon(R.drawable.hdbuilder)
+        .setContentTitle("Record uploaded!")
+        .setContentText("Your score has been uploaded to the online leaderboards!")
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+    // show the notification
+    if (ActivityCompat.checkSelfPermission(
+            context,
+            Manifest.permission.POST_NOTIFICATIONS
+        ) != PackageManager.PERMISSION_GRANTED
+    ) {
+        ActivityCompat.requestPermissions(
+            context as Activity,
+            arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+            PERMISSION_REQUEST_CODE
+        )
+        return
+    }
+    notificationManager.notify(notificationId, builder.build())
+}
+
+private const val PERMISSION_REQUEST_CODE = 1069
