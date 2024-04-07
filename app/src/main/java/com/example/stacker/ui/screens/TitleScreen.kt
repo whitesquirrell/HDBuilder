dcb4d6c8a94d15508cd1554232a2036bbfffa003
JRKhoo <142112507+JRKhoo@users.noreply.github.com>
2024-04-07 22:39:23 +0800

    Added title flavor text running on separate thread with fixed time delta

--- app/src/main/java/com/example/stacker/ui/screens/TitleScreen.kt
+++ app/src/main/java/com/example/stacker/ui/screens/TitleScreen.kt
@@ -16,7 +16,17 @@ import androidx.compose.foundation.background
 import androidx.compose.ui.res.colorResource
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.sp
+import com.example.stacker.ThreadPool
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 @Composable
@@ -38,6 +48,12 @@ fun TitleScreen(navController: NavController) {
                 .fillMaxSize()
                 .offset(y = (-50).dp),
             contentScale = ContentScale.Fit // Ensures the image covers the available space
+        )
+
+        RandomTextDisplay(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 100.dp)
         )
 
 
@@ -86,4 +102,45 @@ fun TitleScreen(navController: NavController) {
             }
         }
     }
+}
+
+object TextData {
+    val textList = listOf(
+        "Make SG Great Again!",
+        "Build The HDBs!",
+        "Make Your Family Proud!",
+        "Do It For Your Country!",
+        "Singapore Is Proud!"
+    )
+}
+@Composable
+fun RandomTextDisplay(
+    modifier: Modifier = Modifier,
+) {
+    val displayText = remember { mutableStateOf(TextData.textList.random()) }
+
+    LaunchedEffect(Unit) {
+        val job = ThreadPool.execute(object : Runnable {
+            override fun run() {
+                while (true) {
+                    val startTime = System.currentTimeMillis()
+                    val endTime = startTime + 5000L // 5 seconds
+
+                    while (System.currentTimeMillis() < endTime) {
+                        Thread.sleep(100) // Sleep for 100 milliseconds
+                    }
+                    displayText.value = TextData.textList.random()
+
+                }
+            }
+        })
+
+    }
+
+    Text(
+        text = displayText.value,
+        color = Color.White,
+        modifier = modifier,
+        fontSize = 30.sp
+    )
 }
